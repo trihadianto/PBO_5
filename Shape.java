d3c1279f159ea829e477423cf6707f6dfb47238e
tri hadianto <114473435+trihadianto@users.noreply.github.com>
2024-03-14 14:32:41 +0700

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,15 @@
+public abstract class Shape {
+    protected double area;
+    protected double perimeter;
+    protected String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void getInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("area: " + area);
+        System.out.println("perimeter: " + perimeter);
+    }
+}
